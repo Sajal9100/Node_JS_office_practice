44b8ffe1259411b92692d4e057c1df62c89a168f
Sajal singh <114485439+Sajal9100@users.noreply.github.com>
2024-10-16 11:21:50 +0000

    upto lecture 5

---
+++ Node-test/index.js
@@ -0,0 +1,21 @@
+const app = require('./app')
+
+
+console.log(app.x);
+
+// var a = 2;
+// var b = 3;
+// var c = 4;
+
+// console.log(a+b+c);
+
+
+// 
+
+let x = 20
+ if (x === 20) {
+   console.log("matched");
+   
+ }
+
+ let result = arr.
